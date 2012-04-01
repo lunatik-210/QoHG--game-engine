5b2641266209624cee9c04ec6091d334bc3298b1
Alexander Gudulin <alexandr.gudulin@gmail.com>
2012-04-02 02:27:56 +0400

    Added landscape sprites

--- main.py
+++ main.py
@@ -2,12 +2,27 @@
 
 import random
 import pygame
-from pygame.color import THECOLORS
 from pygame.locals import *
 from MapGenerator import *
 
 if not pygame.font: print 'Warning, fonts disabled'
 if not pygame.mixer: print 'Warning, sound disabled'
+
+class LandscapeBlock(pygame.sprite.Sprite):
+    """
+    Landscape Block sprite
+    There are 4 types of it:
+        Water < Ground < Grace < Forest
+    """
+    def __init__(self, parent, x, y, w, h, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.topleft = [x, y]
+        self.rect.w, self.rect.h = w, h
+    
+    def draw(self, parent):
+        parent.blit(self.image, self.rect)
+
 
 class Main:
     """
@@ -27,13 +42,20 @@ class Main:
         """Create the Screen"""
         self.screen = pygame.display.set_mode((self.width, self.height))
 
+
     def MainLoop(self):
         """This is the Main Loop of the Game"""
 
-        self.colors = { 0 : THECOLORS["blue"],
-                        1 : THECOLORS["yellow"],
-                        2 : THECOLORS["black"],
-                        3 : THECOLORS["green"] }
+        img_resources = "./images/"
+        img_ground = pygame.image.load(img_resources + "ground.png").convert()
+        img_forest = pygame.image.load(img_resources + "forest.png").convert()
+        img_grace = pygame.image.load(img_resources + "grace.png").convert()
+        img_water = pygame.image.load(img_resources + "water.png").convert()
+
+        self.img_blocks = { 0 : img_water,
+                            1 : img_grace,
+                            2 : img_ground,
+                            3 : img_forest }
         self.block_size = 64
         self.block_size_x = self.width / self.block_size
         self.block_size_y = self.height / self.block_size
@@ -48,19 +70,21 @@ class Main:
 
         while 1:
             for event in pygame.event.get():
-                if event.type == pygame.QUIT: 
+                if event.type == pygame.QUIT:
                     sys.exit()
                 elif event.type == KEYDOWN:
-                    if (event.key == K_RIGHT):
+                    if event.key == K_ESCAPE:
+                        sys.exit()
+                    if event.key == K_RIGHT:
                         self.displs_x += speed
                         self.displs_x %= self.land.getSize()
-                    elif (event.key == K_LEFT):
+                    elif event.key == K_LEFT:
                         self.displs_x -= speed
                         self.displs_x %= self.land.getSize()
-                    elif (event.key == K_UP):
+                    elif event.key == K_UP:
                         self.displs_y -= speed
                         self.displs_y %= self.land.getSize()
-                    elif (event.key == K_DOWN):
+                    elif event.key == K_DOWN:
                         self.displs_y += speed
                         self.displs_y %= self.land.getSize()
                     self.redraw()
@@ -69,15 +93,21 @@ class Main:
         for x in range(self.block_size):
             for y in range(self.block_size):
                 val = self.land.value((x+self.displs_x)%size,(y+self.displs_y)%size)
-                color_id = self.get_color_id(val)
-                pygame.draw.rect( self.screen, self.colors[color_id], 
-                                  (x*self.block_size_x, y*self.block_size_y, self.block_size_x, self.block_size_y))
+                img_block_id = self.get_block_type_id(val)
+                lb = LandscapeBlock(self.screen,
+                                    x*self.block_size_x,
+                                    y*self.block_size_y,
+                                    self.block_size_x,
+                                    self.block_size_y,
+                                    self.img_blocks[img_block_id])
+                lb.draw(self.screen)
+
         pygame.display.flip()
 
-    def get_color_id(self, val):
+    def get_block_type_id(self, val):
         for i in range(len(self.heights)-1):
             if self.heights[i] <= val <= self.heights[i+1]:
-                return i
+                return i        
 
 
 if __name__ == "__main__":
