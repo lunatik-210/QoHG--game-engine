61ad69fce8d35ebf131f074a0d5a86bd0bc0198b
Andrew Lapin <andrew.d.lapin@gmail.com>
2012-04-10 19:27:21 +0400

    Changed author name

--- land.py
+++ land.py
@@ -62,3 +62,37 @@ class Land:
 
     def load(self, name):
         pass
+
+
+class Position:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def __str__(self):
+        return "(%d, %d)" % (self.x, self.y)
+
+    def __eq__(self, pos):
+        return ( self.x == pos.x and 
+                 self.y == pos.y )
+
+    def __repr__(self):
+        return "(%d,%d)" % (self.x, self.y)
+
+    def left(self):
+        return Position(self.x-1, self.y)
+
+    def right(self):
+        return Position(self.x+1, self.y)
+
+    def bottom(self):
+        return Position(self.x, self.y+1)
+
+    def top(self):
+        return Position(self.x, self.y-1)
+
+    def value(self):
+        return (self.x, self.y)
+
+    def get_neighbors(self):
+        return (self.left(), self.top(), self.right(), self.bottom())
