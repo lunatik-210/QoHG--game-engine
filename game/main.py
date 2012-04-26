92a79ff6f8ffc8b3572eabe5becfc1acf522810e
Andrew Lapin <andrew.d.lapin@gmail.com>
2012-04-26 21:32:42 +0400

    Added config file with bioms defs

--- game/main.py
+++ game/main.py
@@ -21,6 +21,7 @@ import lands.generators.Map as MapGenerator
 from lands.Land import Land
 from lands.DemoLand import DemoLand
 from lands.Position import Position
+import config
 
 #from pathsearch import a_star_path_search as get_path
 #################################
@@ -182,17 +183,17 @@ class Main:
         img_player = self.load_image_with_alpha("player%s%d.png" % (suffix, self.texture_size))
         img_golem  = self.load_image_with_alpha("golem%s%d.png" % ('__', self.texture_size))
         
-        self.img_blocks = { objects['water']  : img_water,
-                            objects['sand']   : img_sand,
-                            objects['grass']  : img_grass,
-                            objects['log']    : img_log,
-                            objects['stone']  : img_stone,
-                            objects['tree']   : img_tree,
-                            objects['snow']   : img_snow,
-                            monsters['wolf']  : img_wolf,
-                            monsters['pig']   : img_pig,
-                            monsters['golem'] : img_golem,
-                            player_id : img_player }
+        self.img_blocks = { config.objects['water']  : img_water,
+                            config.objects['sand']   : img_sand,
+                            config.objects['grass']  : img_grass,
+                            config.objects['log']    : img_log,
+                            config.objects['stone']  : img_stone,
+                            config.objects['tree']   : img_tree,
+                            config.objects['snow']   : img_snow,
+                            config.monsters['wolf']  : img_wolf,
+                            config.monsters['pig']   : img_pig,
+                            config.monsters['golem'] : img_golem,
+                            config.player_id : img_player }
                 
     def load_image(self, name):
         img_resources = "./resources/images/"
@@ -246,147 +247,10 @@ class Main:
         # just fill the surface
         for x in range(border, s-border):
             for y in range(border, s-border):
-                color = pygame.Color(colors[demo[x][y]])
+                color = pygame.Color(config.colors[demo[x][y]])
                 pygame.draw.rect(map, color, pygame.Rect(x*ds, y*ds, ds, ds))
         return map
 
-
-##################################################
-# types of objects
-##################################################
-objects = {
-    'water' : 0,
-    'sand'  : 1,
-    'grass' : 2,
-    'log'   : 3,
-    'stone' : 4,
-    'tree'  : 5,
-    'snow'  : 6,
-}
-
-monsters = {
-    'wolf'  : 11,
-    'pig'   : 12,
-    'golem' : 13    
-}
-
-player_id = 10
-
-##################################################
-# colors for mini/demo map
-# id - color
-##################################################
-colors = {
-    objects['water']  : 'Blue',
-    objects['sand']   : 'Yellow',
-    objects['grass']  : 'Black',
-    objects['log']    : 'Brown',
-    objects['stone']  : 'Gray',
-    objects['tree']   : 'Green',
-    objects['snow']   : 'White',
-    monsters['wolf']  : 'Black',
-    monsters['pig']   : 'Black',
-    monsters['golem'] : 'Black',
-    player_id         : 'Black'
-}
-
-#################################################
-# types of bioms (areas)
-##################################################
-bioms = {
-    'prairie'   : 0,
-    'mountains' : 1,
-    'desert'    : 2,
-    'swamp'     : 3,
-    'taiga'     : 4
-}
-
-##################################################
-# determine hight map for bioms
-##################################################
-humidity = (((0.00, 0.50), bioms['prairie']   ),
-            ((0.50, 0.65), bioms['mountains'] ),
-            ((0.65, 0.80), bioms['taiga']     ),
-            ((0.80, 0.90), bioms['swamp']     ),
-            ((0.90, 1.00), bioms['desert']    ))
-
-##################################################
-# Determine hight map for every biom 
-##################################################
-taiga = { 'objects' : (((0.00, 0.58), objects['water'] ),
-                       ((0.58, 0.60), objects['sand']  ),
-                       ((0.60, 0.62), objects['grass'] ),
-                       ((0.62, 0.64), objects['tree']  ),
-                       ((0.64, 0.78), objects['grass'] ),
-                       ((0.78, 0.80), objects['tree']  ),
-                       ((0.80, 0.94), objects['grass'] ),
-                       ((0.94, 1.00), objects['tree']  )),
-         'monsters' : ((0.1, monsters['wolf']),
-                       (0.1, monsters['pig']),
-                       (0.8, objects['grass'])),
-         'default'  : objects['grass']
-}
-
-prairie = { 'objects' : (((0.00, 0.58),  objects['water'] ),
-                         ((0.58, 0.60),  objects['sand']  ),
-                         ((0.60, 0.94),  objects['grass'] ),
-                         ((0.94, 1.00),  objects['tree']  )),
-          'monsters' : ((0.2, monsters['pig']),
-                        (0.8, objects['grass'])),
-          'default'  : objects['grass']
-}
-
-mountains = { 'objects' : (((0.00, 0.58),  objects['water'] ),
-                           ((0.58, 0.60),  objects['sand']  ),
-                           ((0.60, 0.70),  objects['grass'] ),
-                           ((0.70, 0.73),  objects['stone'] ),
-                           ((0.73, 0.80),  objects['grass'] ),
-                           ((0.80, 0.85),  objects['stone'] ),
-                           ((0.85, 0.88),  objects['snow']  ),
-                           ((0.88, 0.91),  objects['tree']  ),
-                           ((0.91, 0.97),  objects['snow']  ),
-                           ((0.97, 1.00),  objects['stone'] )),
-             'monsters' : ((0.2, monsters['golem']),
-                           (0.8, objects['snow'])),
-             'default'  : objects['snow']
-}
-
-desert = { 'objects' :  (((0.00, 0.58), objects['water'] ),
-                         ((0.58, 0.59), objects['sand']  ),
-                         ((0.59, 0.61), objects['grass'] ),
-                         ((0.61, 1.00), objects['sand']  )),
-           'monsters' : None,
-           'default'  : objects['sand']
-}        
-
-swamp = { 'objects' : (((0.00, 0.56), objects['water'] ),
-                       ((0.56, 0.58), objects['sand']  ),
-                       ((0.58, 0.77), objects['grass'] ),
-                       ((0.77, 0.79), objects['log']   ),
-                       ((0.79, 0.82), objects['tree']  ),
-                       ((0.82, 0.94), objects['grass'] ),
-                       ((0.94, 0.97), objects['log']   ),
-                       ((0.97, 0.98), objects['tree']  ),
-                       ((0.98, 1.00), objects['log']   )),
-         'monsters' : None,
-         'default'  : objects['grass']
-}      
-
-##################################################
-# Set hight map
-##################################################
-config = {
-    'humidity' : humidity,
-    bioms['prairie'] : prairie,
-    bioms['mountains'] : mountains,
-    bioms['desert'] : desert,
-    bioms['swamp'] : swamp,
-    bioms['taiga'] : taiga,
-    'default' : objects['grass'],
-    'monsters' : monsters,
-    'objects' : objects,
-    'bioms' : bioms
-}
 
 ##################################################
 # define constants
@@ -397,11 +261,7 @@ size = 2000
 # (change view) roughness, more biggest value will give more filled map
 roughness = 20.0
 # (change map ) you can think about seed as map number or id
-land_id = 1233213
-
-allowable_list = (objects['sand'],
-                  objects['grass'],
-                  objects['snow'])
+land_id = 1233215
 ##################################################
 
 def start(fullscreen_option=True, debug_option=False):
@@ -409,7 +269,7 @@ def start(fullscreen_option=True, debug_option=False):
     map_generator = MapGenerator.DiamondSquare(size, roughness, land_id, True)
 
     # init land
-    land = Land(config, player_id, map_generator, allowable_list)
+    land = Land(config.config, config.player_id, map_generator, config.allowable_list)
 
     # create window
     MainWindow = Main(land, 1024, 768, debug_option)
